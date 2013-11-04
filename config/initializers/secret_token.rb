eb02463dee9e611bc533c31f87eeb94aac52427b
gogotanaka <yakuruto771@live.jp>
2013-11-04 20:12:37 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockhigh::Application.config.secret_key_base = '7a2c06791bd12fc6f777d5f5b18e1b5b181cd9cbe4fcbc2acff5f43e8380da812fb72106c729e2f0eb1889a6ea8b141f3ae01e6ba2300dc769c53759a0e77e9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stockhigh::Application.config.session_store :cookie_store, key: '_stockhigh_session'
